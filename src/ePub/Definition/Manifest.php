861150ffd7b5f3893903735cb0a30c6b306f1d4e
Justin Rainbow <justin.rainbow@gmail.com>
2011-12-30 19:00:10 -0700

    More custom Exception classes

--- src/ePub/Definition/Manifest.php
+++ src/ePub/Definition/Manifest.php
@@ -13,7 +13,31 @@ namespace ePub\Definition;
 
 use ePub\Definition\Metadata;
 use ePub\Definition\ManifestItem;
+use ePub\Exception\DuplicateItemException;
 
 class Manifest extends Collection
 {
+    /**
+     * @var array
+     */
+    private $resources = array();
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add(ManifestItem $item)
+    {
+        $id = $item->getIdentifier();
+
+        $href = $item->href;
+
+        if (isset($this->resources[$href])) {
+            throw new DuplicateItemException(
+                'A single resource (href) must not be listed in the manifest more than once'
+            );
+        }
+
+        $this->resources[$href] = $item;
+        $this->items[$id]       = $item;
+    }
 }
