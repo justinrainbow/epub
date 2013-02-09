b811711a4fd962c32b12a1f58a0f92b75d92e5ef
Julien Chaumond <chaumond@gmail.com>
2013-02-09 17:40:41 +0100

    Test Epub versions, plus some refactoring

--- tests/ePub/Tests/ReaderTest.php
+++ tests/ePub/Tests/ReaderTest.php
@@ -23,24 +23,27 @@ class ReaderTest extends BaseTest
 
     public function testLoadingEpubFile()
     {
-        $fixture = $this->getFixturePath('the_velveteen_rabbit.epub');
-
-        $reader = new Reader();
-        $epub = $reader->load($fixture);
-
+        $epub = $this->getFixtureEpub('the_velveteen_rabbit.epub');
+        
         $this->assertTrue($epub instanceof \ePub\Definition\Package);
     }
 
     public function testReadingManifestItemContent()
     {
-        $fixture = $this->getFixturePath('the_velveteen_rabbit.epub');
-
-        $reader = new Reader();
-        $epub   = $reader->load($fixture);
+        $epub = $this->getFixtureEpub('the_velveteen_rabbit.epub');
 
         $manifest   = $epub->getManifest();
         $dedication = $manifest->get('dedication');
         $expected   = $this->getFixture('the-velveteen-rabbit/' . $dedication->href);
         $this->assertEquals($expected, $dedication->getContent());
+    }
+    
+    public function testReadingEpubVersion()
+    {
+        $epub = $this->getFixtureEpub('epub3_nested_nav.epub');
+        $this->assertEquals('3.0', $epub->version);
+        
+        $epub = $this->getFixtureEpub('the_velveteen_rabbit.epub');
+        $this->assertEquals('2.0', $epub->version);
     }
 }
